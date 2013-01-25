c4e439a27e354af3fd5173f22da36ce4bc153ee7
Haiyang He <hyhe@thoughtworks.com>
2013-01-25 21:00:39 +0800

    init

---
+++ init.sh
@@ -0,0 +1,7 @@
+
+#!/bin/bash
+wget http://mirror.bjtu.edu.cn/apache/hadoop/common/hadoop-1.1.1/hadoop-1.1.1.tar.gz
+tar -xzvf hadoop-1.1.1.tar.gz
+sudo apt-get update
+sudo apt-get install openjdk-6-jdk
+sudo apt-get install openssh-server
